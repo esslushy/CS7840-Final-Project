4de646583963161a93ee6310fa98d348b2b1b71d
Claire Schlesinger <esslushy@gmail.com>
2026-05-13 14:48:29 -0400

    add mnist font dataset

--- src/main.py
+++ src/main.py
@@ -3,36 +3,35 @@ import torchvision
 import torchvision.transforms as transforms
 import torch.nn as nn
 import torch.optim as optim
-from Models import CNN, ViT, NaiveNet
 from utils import equiv_error_calc, baseline_cka_computation
 import json
 from argparse import ArgumentParser
 from pathlib import Path
+import sys
+from print_digit import load_mnist_font
 
-CLASSES = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+if "mnist_font" in sys.argv:
+    CLASSES = tuple(range(10))
+else:
+    CLASSES = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
 NUM_EPOCHS = 200
 BATCH_SIZE = 64
 
-def main(model: str, kernel: str, rotation: bool, holdout: str, thicker: bool, finetune: Path):
-    transform_operations = [transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
-    if rotation and not holdout:
-        transform_operations.append(transforms.RandomRotation(degrees=(0, 360)))
-    transform_train = transforms.Compose(transform_operations)
-
+def main(model: str, dataset: str, kernel: str, rotation: bool, holdout: str, thicker: bool, finetune: Path):
     device = "cuda" if torch.cuda.is_available() else "cpu"
 
-    trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
-                                            download=True, transform=transform_train)
+    if dataset == "cifar":
+        from CIFARNets import CNN, ViT, NaiveNet
+        trainset, testset = load_cifar(rotation, holdout)
+    elif dataset == "mnist_font":
+        from MNISTNets import CNN, ViT, NaiveNet
+        trainset, testset = load_mnist_font()
+    else:
+        raise Exception("Unknown Dataset")
     trainloader = torch.utils.data.DataLoader(trainset, batch_size=BATCH_SIZE,
                                             shuffle=True, num_workers=2)
-    transform_test = transforms.Compose([
-        transforms.ToTensor(), 
-        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-    ])
-
-    testset = torchvision.datasets.CIFAR10(root='./data', train=False,
-                                        download=True, transform=transform_test)
-    testloader = torch.utils.data.DataLoader(testset, batch_size=10000, # Get all of the images.
+    testloader = torch.utils.data.DataLoader(testset, batch_size=len(testset), # Get all of the images.
                                             shuffle=False, num_workers=2)
     test_images, test_labels = next(iter(testloader))
     test_images = test_images.to(device)
@@ -96,8 +95,27 @@ def main(model: str, kernel: str, rotation: bool, holdout: str, thicker: bool, f
 
     torch.save(net.state_dict(), f"models/cifar_{model}.pth")
 
-    with open(f"results/{'learned_equivariant' if rotation else 'non_equivariant'}_{model}{'_thicker' if thicker else ''}_kernel_{kernel}{f'_holdout_{holdout}' if holdout else ''}{'_finetuned' if finetune else ''}_statistics.json", "wt+") as f:
+    with open(f"results/{'learned_equivariant' if rotation else 'non_equivariant'}_{model}{'_thicker' if thicker else ''}_dataset_{dataset}_kernel_{kernel}{f'_holdout_{holdout}' if holdout else ''}{'_finetuned' if finetune else ''}_statistics.json", "wt+") as f:
         json.dump(statistics, f)
+
+def load_cifar(rotation, holdout):
+    transform_operations = [transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
+    if rotation and not holdout:
+        transform_operations.append(transforms.RandomRotation(degrees=(0, 360)))
+    transform_train = transforms.Compose(transform_operations)
+
+    trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                            download=True, transform=transform_train)
+    transform_test = transforms.Compose([
+        transforms.ToTensor(), 
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
+
+    testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                        download=True, transform=transform_test)
+
+                                            
+    return trainset, testset
 
 def update_statistics(kernel, test_images, test_labels, net, criterion, statistics, trainloader, device):
     net.train()
@@ -127,6 +145,7 @@ def accuracy(output, labels):
 if __name__ == "__main__":
     args = ArgumentParser()
     args.add_argument("--model", help="Which model to use", type=str, choices=("cnn", "naive", "vit"), default="cnn")
+    args.add_argument("--dataset", help="The dataset to train on.", type=str, choices=("cifar", "mnist_font"), default="cifar")
     args.add_argument("--kernel", help="Which kernel to use for CKA", type=str, choices=["rbf", "linear"], default="linear")
     args.add_argument("--rotation", help="Whether to train with rotation applied", action="store_true")
     args.add_argument("--holdout", help="The class to hold out from rotation", type=str, choices=CLASSES)
@@ -140,4 +159,4 @@ if __name__ == "__main__":
     if args.model == "naive" and args.thicker:
         raise Exception("Can't make a thicker naive model.")
     
-    main(args.model, args.kernel, args.rotation, args.holdout, args.thicker, args.finetune)
+    main(args.model, args.dataset, args.kernel, args.rotation, args.holdout, args.thicker, args.finetune)
---
+++ src/print_digit/__init__.py
@@ -0,0 +1,51 @@
+import os
+import cv2
+import numpy as np
+from torch.utils.data import Dataset, SubsetRandomSampler
+from PIL import Image
+import torch
+from tqdm import tqdm
+from torchvision.transforms import ToTensor
+
+def load_mnist_font():
+    X = []
+    Y = []
+    for i in range(10):
+        for d in os.listdir("print_digit/dataset/assets/{}".format(i)):
+            t_img = cv2.imread("print_digit/dataset/assets/{}".format(i)+"/"+d)
+            t_img = cv2.cvtColor(t_img,cv2.COLOR_BGR2GRAY)
+            X.append(t_img)
+            Y.append(i)
+
+    X = np.array(X, dtype=np.float32) / 255.
+    Y = np.array(Y)
+    perm = np.random.permutation(X.shape[0])
+    X = X[perm]
+    Y = Y[perm]
+
+    totensor = ToTensor()
+    mnist_train = PrintedDigitDataset(X, Y, mode='train', transform=totensor)
+    mnist_test = PrintedDigitDataset(X, Y, mode='test', transform=totensor)
+    return mnist_train, mnist_test
+
+class PrintedDigitDataset(Dataset):
+    def __init__(self, X, Y, mode, transform=None):
+        assert mode in ['train', 'test', 'valid']
+        self.transform = transform
+        if mode == 'train':
+            self.data = X[:5000].astype(np.float32)
+            self.targets = Y[:5000].astype(np.int64)
+        else:
+            self.data = X[5000:6000].astype(np.float32)
+            self.targets = Y[5000:6000].astype(np.int64)
+        self.num_samples = len(self.targets)
+
+    def __getitem__(self, index):
+        image, label = self.data[index], self.targets[index]
+        image = Image.fromarray(image)
+        if self.transform is not None:
+            image = self.transform(image)
+        return image, label
+
+    def __len__(self):
+        return len(self.data)
